5d89683301508bfc7d7d0dcef92c8fedd056e12c
Mike Koss <mckoss@startpad.org>
2011-12-31 18:47:09 -0800

    use css file for pdf formatting

--- bin/make-pdf.sh
+++ bin/make-pdf.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 BINDIR=$(cd `dirname $0` && pwd)
 
-cat format.html > $1.html
-$BINDIR/markdown.pl $1.md >> $1.html
-pisa $1.html
+$BINDIR/markdown.pl $1.md > $1.html
+pisa --css=default.css $1.html
 # htmldoc --links --webpage --footer format=pdf -f $1.pdf $1.html
