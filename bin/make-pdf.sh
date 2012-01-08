4a3cbd77b7652d87e6e47e96b5e920cd3478fd44
Mike Koss <mckoss@startpad.org>
2012-01-07 18:41:40 -0800

    add epub and error check

--- bin/make-pdf.sh
+++ bin/make-pdf.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 BINDIR=$(cd `dirname $0` && pwd)
 
+if [ "$1" == "" ]; then
+    echo "Usage: $0 base-name"
+    echo "Produces base-name.html and base-name.pdf from base-name.md."
+    exit 1
+fi
+
 $BINDIR/Markdown.pl $1.md > $1.html
 pisa -s --encoding=utf8 --css=default.css $1.html
 # htmldoc --links --webpage --footer format=pdf -f $1.pdf $1.html
--- bin/make-tex-pdf.sh
+++ bin/make-tex-pdf.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 BINDIR=$(cd `dirname $0` && pwd)
 
+if [ "$1" == "" ]; then
+    echo "Usage: $0 base-name"
+    echo "Produces base-name.tex, base-name.pdf, and base-name.epub from base-name.md."
+    exit 1
+fi
+
 pandoc -f markdown -t latex --template=template.tex $1.md -o $1.tex
+pandoc -f markdown -t epub --template=template.tex $1.md -o $1.epub
 markdown2pdf $1.md --template=template.tex -o $1.pdf
 
 # $BINDIR/Markdown.pl $1.md > $1.html
